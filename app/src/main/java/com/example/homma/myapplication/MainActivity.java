a601111b8111b4dedb3a533e3d7499666cc7e532
fujimoto_t <develop.fujimoto@gmail.com>
2018-03-21 22:20:16 +0900

    4つのタブを作成

--- app/src/main/java/com/example/homma/myapplication/MainActivity.java
+++ app/src/main/java/com/example/homma/myapplication/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.homma.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, PageFragment.OnFragmentInteractionListener
+{
+    private static final String[] pageTitle = {"NEW!", "イベントを探す", "お気に入り", "マイページ"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("EVENT's");
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+            @Override
+            public Fragment getItem(int position)
+            {
+                return PageFragment.newInstance(position + 1);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position)
+            {
+                return pageTitle[position];
+            }
+
+            @Override
+            public int getCount()
+            {
+                return pageTitle.length;
+            }
+        };
+
+        viewPager.setAdapter(fragmentPagerAdapter);
+        viewPager.addOnPageChangeListener(this);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {}
+
+    @Override
+    public void onPageScrollStateChanged(int state) {}
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {}
 }
